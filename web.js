5058404b58d035f281ec56bababfd52331cdad46
Aatish Mandelecha <aatish.mandelecha@gmail.com>
2013-07-12 06:03:30 +0000

    Making changes in web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Yo Bro!!');
+  var result = fs.readFileSync('index.html');
+  response.send(result);
 });
 
 var port = process.env.PORT || 5000;
