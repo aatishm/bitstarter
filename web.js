c3ee1cd7a02a4ba6f524c3ac2442f742d4136f07
aatishm <aatish.mandelecha@gmail.com>
2013-11-13 06:13:57 +0000

    Make development box https

--- web.js
+++ web.js
@@ -11,11 +11,22 @@ var shortId = require('shortid');
 var expressValidator = require('express-validator');
 var uuid = require('node-uuid');
 
+// Created a self-signed certificate following this process: https://devcenter.heroku.com/articles/ssl-certificate-self
+// TODO: To get a SSL cert from a provider, follow: https://devcenter.heroku.com/articles/ssl-endpoint
+// TODO: Currently only local box is https since server key/crt is generated for it
+var httpsOptions = {};
+if (process.env.SERVER_KEY && process.env.SERVER_CRT) {
+    var privateKey = fs.readFileSync('/home/ubuntu/server.key').toString();
+    var certificate = fs.readFileSync('/home/ubuntu/server.crt').toString();
+    httpsOptions = {key: privateKey, cert: certificate};
+}
+
 // Set seed for shortId
 shortId.seed(193523723);
 
 // Configure common middlewares. Precedence matters. Think of middleware as a stack of handlers that need to be executed one after another for every HTTP Request
 var app = express.createServer(
+    httpsOptions,
     express.logger(),
     // Use app.use('/static', middleware) so that express.static is not called on every request. See SO below:
     // 1) http://stackoverflow.com/questions/12695591/node-js-express-js-how-does-app-router-work
